fb885ea0a63aec460ddfada76c65e58be3574f71
blake_zuo <zuoxc@ruc.edu.cn>
2019-06-06 22:24:06 +0800

    strong

--- src/search_engine/diversify.py
+++ src/search_engine/diversify.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # 获取results列表中所有document的caption
 # 为降低运算复杂度对document进行切分，切分尺寸为length
-def GetCaption(docs, results, query_terms, length = 100):
+def GetCaption(docs, results, query_terms, length = 88):
 	captions = dict()
 	for item in results:
 		docid = item[0]
@@ -25,10 +25,21 @@ def GetCaption(docs, results, query_terms, length = 100):
 					count[int(loc / length)] += 1
 					last = loc + 1
 		idx = count.index(max(count))
-		start = min(content.find('，',idx), content.find('。',idx)) + 1
-		end = content.find('。',idx + length) + 1
-		if end == -1:
-			end = len(content)
-		captions[docid] = content[start: end]
-	return captions
 
+		start = idx * length
+		while content[start] not in ['，',',','。','.','!','！',';','；'] and start > -1:
+			start -= 1
+		start += 1
+		end = min(start + length, len(content))
+		content = content[start: end]
+		for term in query_terms:
+			last = 0
+			while True:
+				loc = content.find(term, last)
+				if loc == -1:
+					break
+				else:
+					content = content[:loc] + "<strong>" + content[loc:loc+len(term)] + "</strong>" + content[loc+len(term):]
+					last = loc + 17 + len(term)
+		captions[docid] = content
+	return captions
